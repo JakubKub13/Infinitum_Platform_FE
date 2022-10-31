8c1938f2253bc41ce45d8c2749a47b580e05d8fa
JakubKub13 <jakubkubala3@gmail.com>
2022-10-31 12:20:41 +0100

    Setting up state variables

--- src/app/infinitum.service.ts
+++ src/app/infinitum.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ethers, BigNumber } from 'ethers'
+import { ethers } from 'ethers'
 import bigNumberToETHString from '../helpers/bigNumberToETHString'
 import * as ContractAddressesJSON from '../assets/contracts/contracts.json'
+import * as InfinitumFarmJSON from '../assets/contracts/infinitumFarm-contract/InfinitumFarm.json'
+import * as InfinitumTokenJSON from '../assets/contracts/infinitumToken-contract/infinitumToken.json'
+import currentEpoch from 'src/helpers/currentEpoch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfinitumService {
+  currentAccount: string;
+  isLoggedIn: Boolean;
+  contractOwner: string;
+  infinitumFarmContractAddress: string;
+  infinitumTokenContractAddress: string;
 
-  constructor() { }
+  contractAddressesJSON {
+    infinitumFarmContractAddress: string,
+    infinitumTokenContractAddress: string
+  }
+  infinitumFarmContractJSON: any
+  infinitumTokenContractJSON: any
+
+  provider: ethers.providers.BaseProvider
+
+  constructor() {
+    this.currentAccount: "";
+    this. ///continue here
+
+   }
 }
